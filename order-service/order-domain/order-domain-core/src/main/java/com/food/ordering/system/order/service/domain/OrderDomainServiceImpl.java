fa58eee974ca39172bdc9d0f6b293587427919ec
krost67 <jeka113381@gmail.com>
2023-05-26 20:51:21 +0300

    create order domain events; prepare OrderDomainService

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
@@ -0,0 +1,37 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvent;
+import java.util.List;
+
+public class OrderDomainServiceImpl implements OrderDomainService {
+
+    @Override
+    public OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant) {
+
+        return null;
+    }
+
+    @Override
+    public OrderPaidEvent payOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public void approveOrder(Order order) {
+
+    }
+
+    @Override
+    public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
+        return null;
+    }
+
+    @Override
+    public void cancelOrder(Order order, List<String> failureMessages) {
+
+    }
+}

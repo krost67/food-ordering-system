2334a1cbcd473bd950141843ee845e29276d491c
krost67 <jeka113381@gmail.com>
2023-08-16 23:00:01 +0300

    add container module for restaurant service

--- payment-service/payment-container/src/main/resources/init-schema.sql
+++ payment-service/payment-container/src/main/resources/init-schema.sql
@@ -12,12 +12,12 @@ DROP TABLE IF EXISTS "payment".payments CASCADE;
 
 CREATE TABLE "payment".payments
 (
-    id uuid NOT NULL,
-    customer_id uuid NOT NULL,
-    order_id uuid NOT NULL,
-    price numeric(10,2) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    status payment_status NOT NULL,
+    id          uuid                     NOT NULL,
+    customer_id uuid                     NOT NULL,
+    order_id    uuid                     NOT NULL,
+    price       numeric(10, 2)           NOT NULL,
+    created_at  TIMESTAMP WITH TIME ZONE NOT NULL,
+    status      payment_status           NOT NULL,
     CONSTRAINT payments_pkey PRIMARY KEY (id)
 );
 
@@ -25,9 +25,9 @@ DROP TABLE IF EXISTS "payment".credit_entry CASCADE;
 
 CREATE TABLE "payment".credit_entry
 (
-    id uuid NOT NULL,
-    customer_id uuid NOT NULL,
-    total_credit_amount numeric(10,2) NOT NULL,
+    id                  uuid           NOT NULL,
+    customer_id         uuid           NOT NULL,
+    total_credit_amount numeric(10, 2) NOT NULL,
     CONSTRAINT credit_entry_pkey PRIMARY KEY (id)
 );
 
@@ -39,9 +39,9 @@ DROP TABLE IF EXISTS "payment".credit_history CASCADE;
 
 CREATE TABLE "payment".credit_history
 (
-    id uuid NOT NULL,
-    customer_id uuid NOT NULL,
-    amount numeric(10,2) NOT NULL,
-    type transaction_type NOT NULL,
+    id          uuid             NOT NULL,
+    customer_id uuid             NOT NULL,
+    amount      numeric(10, 2)   NOT NULL,
+    type        transaction_type NOT NULL,
     CONSTRAINT credit_history_pkey PRIMARY KEY (id)
 );
